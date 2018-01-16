08bd644bb8ba1efa6e9615bad42e9ac955f98009
Thom David <thomdavid74@gmail.com>
2018-01-16 09:10:02 -0800

    openweathermap api added

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 "use strict";
 
 //variables defined in init.js file
-searchButton.addEventListener('click', searchWeather);
+searchButton.addEventListener('click', searchWeather); //add event listener to search button, call function
 
 function searchWeather() {
-    console.log(searchCity.value); //testing purposes only
+    var cityName = searchCity.value; //store user input from city field
+    var http = new XMLHttpRequest();
+    var apiKey = ""; //user key from openweathermap.org
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName;
+    var method = 'GET';
+
+    if (cityName.trim().length == 0) { //make sure there is an entry in the text field
+        return alert('Please enter a city name');
+    }
+
+    http.open(method, url);
+    http.onreadystatechange = function() {
+        if(http.readyState == XMLHttpRequest.DONE && http.status === 200) {
+
+        } else if (http.readyState === XMLHttpRequest.DONE ) {
+            alert("Something went wrong. Please try again");
+        }
+    }
 }
