2df385289a07fb7564a239fa3f2035a212b56b53
Thom David <thomdavid74@gmail.com>
2018-01-16 08:43:22 -0800

    Test JS functionality

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+//variables defined in init.js file
+searchButton.addEventListener('click', searchWeather);
+
+function searchWeather() {
+    console.log(searchCity.value); //testing purposes only
+}
